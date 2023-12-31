2ac4cd6e193a2d6c8977ac6f248bd4e3d1217ad1
Korab <korabimeri91@gmail.com>
2023-12-31 11:36:00 +0100

    style:changed background on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
   // bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800
 
   return (
-    <main className="flex relative h-full flex-col items-center justify-center">
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]" />
-      <div className="space-y-6 text-center">
+    <main className="flex relative h-full flex-col items-center bg-slate-950 justify-center">
+      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]"></div>
+      <div className="space-y-6 z-10 text-center">
         <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md", font.className)}>
           🔐Auth
         </h1>
