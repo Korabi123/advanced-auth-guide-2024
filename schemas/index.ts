e91717f9d9d0be2419ca406e6342cffb154807d8
Korab <korabimeri91@gmail.com>
2023-12-31 15:11:38 +0100

    api:added register schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,17 @@ export const LoginSchema = z.object({
     message: "Password is required",
   }),
 })
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters",
+  }).regex(new RegExp("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%&]).{8,32}"), {
+    message: "Password must contain one uppercase letter, one lowercase letter, one number and one of the following characters: * . ! @ $ % &",
+  }),
+  name: z.string().min(1, {
+    message: "Name is required",
+  })
+})
