54790e466d95b0978ffda735280baba87703b727
Korab <korabimeri91@gmail.com>
2023-12-31 11:59:30 +0100

    other:created a social button component

---
+++ components/auth/social.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+import { Button } from "@/components/ui/button";
+
+export const Social = () => {
+  return (
+    <div className="flex items-center w-full gap-x-2">
+      <Button 
+        variant="outline" 
+        className="w-full"
+        size="lg"
+        onClick={() => {}}
+      >
+        <FcGoogle className="h-5 w-5" />
+      </Button>
+      <Button variant="outline" className="w-full">
+        <FaGithub className="h-5 w-5" />
+      </Button>
+    </div>
+  )
+}
