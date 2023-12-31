3f309aa6f6b7c97b93bb9f2259a184c624cda96f
Korab <korabimeri91@gmail.com>
2023-12-31 11:58:55 +0100

    other:created a back button component

---
+++ components/auth/back-button.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+interface BackButtonProps {
+  href: string;
+  label: string;
+}
+
+export const BackButton = ({ 
+  href, 
+  label 
+}: BackButtonProps) => {
+  return (
+    <Button
+      variant={"link"}
+      className="font-normal w-full"
+      size={"sm"}
+      asChild
+    >
+      <Link href={href}>
+        {label}
+      </Link>
+    </Button>
+  )
+}
