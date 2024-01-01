473639686ed33e09b840ac19e6eb047a046fc5e7
Korab <korabimeri91@gmail.com>
2024-01-01 09:45:43 +0100

    feat:added actual register functionality

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,12 @@
 "use server";
 
+import { db } from "@/lib/db";
+
+import bcrypt from "bcrypt";
 import * as z from "zod";
 
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -11,5 +15,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" }
   }
 
-  return { success: "Confirmation Email Sent" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email is already taken" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    }
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created" };
 }
