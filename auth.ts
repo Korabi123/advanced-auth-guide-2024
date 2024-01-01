eb2cb4537d600b72bdc22140c548e19b234a0b55
Korab <korabimeri91@gmail.com>
2024-01-01 21:13:15 +0100

    feat:added functionality to login or register with OAuth providers

--- auth.ts
+++ auth.ts
@@ -12,6 +12,20 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: {
+          emailVerified: new Date,
+        }
+      })
+    }
+  },
   callbacks: {
     async session({ token, session }) {
       
