5cace40801c24db176ac3c48bd66dc894d0b7fa1
Korab <korabimeri91@gmail.com>
2024-01-01 21:15:58 +0100

    feat:added the actual onClick function to trigger the OAuth login / register

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,18 +1,24 @@
 "use client";
 
+import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, { callbackUrl: DEFAULT_LOGIN_REDIRECT });
+  }
+
   return (
     <div className="flex items-center w-full gap-x-2">
       <Button 
         variant="outline" 
         className="w-full"
         size="lg"
-        onClick={() => {}}
+        onClick={() => onClick("google")}
       >
         <FcGoogle className="h-5 w-5" />
       </Button>
@@ -20,7 +26,7 @@ export const Social = () => {
         variant="outline" 
         className="w-full"
         size="lg"
-        onClick={() => {}}
+        onClick={() => onClick("github")}
       >
         <FaGithub className="h-5 w-5" />
       </Button>
