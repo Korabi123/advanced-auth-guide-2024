c8dac802d56d2676a60941a41ac759f4ae99097e
Korab <korabimeri91@gmail.com>
2024-01-01 13:23:02 +0100

    other:added protected route

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,25 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+
+const SettingsPage = async () => {
+  const session = await auth();
+
+  return (
+    <>
+      <div className="font-semibold text-center text-6xl">Hello Settings</div>
+      <hr className="my-4" />
+      <div className="font-semibold text-4xl">Session: <br /> {JSON.stringify(session)}</div>
+      <form action={async () => {
+        "use server";
+
+        await signOut();
+      }}>
+        <Button type="submit" size={"lg"} className="mt-4">
+          Sign Out
+        </Button>
+      </form>
+    </>
+  );
+}
+ 
+export default SettingsPage;
