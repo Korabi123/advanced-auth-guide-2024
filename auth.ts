edca0d3078a3f9e3945406deef9c17ace6db6d96
Korab <korabimeri91@gmail.com>
2024-01-01 19:26:39 +0100

    feat:added custom callbacks which enable to add custom fields to session and jwt

--- auth.ts
+++ auth.ts
@@ -1,8 +1,10 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
 
-import { db } from "./lib/db"
-import authConfig from "./auth.config"
+import { getUserById } from "./data/user";
+import { db } from "./lib/db";
+import authConfig from "./auth.config";
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +12,31 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" }, 
   ...authConfig,
