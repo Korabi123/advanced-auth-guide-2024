d346d42dc3774ba03e7b730124e67d6f15f48d94
Korab <korabimeri91@gmail.com>
2024-01-01 13:23:37 +0100

    chore:remove un-neccesary comment

--- middleware.ts
+++ middleware.ts
@@ -37,7 +37,6 @@ export default auth((req) => {
   return null;
 })
 
-// Optionally, don't invoke Middleware on some paths
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
