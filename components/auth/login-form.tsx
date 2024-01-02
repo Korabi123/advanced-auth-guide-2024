2ed8bffecf9217181c3c463b6e7bdbcbc5c1f63f
Korab <korabimeri91@gmail.com>
2024-01-02 13:11:10 +0100

    style:added email sent message when trying to log in

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -48,8 +48,7 @@ export const LoginForm = () => {
       login(values)
         .then((data) => {
           setError(data?.error);
-          // TODO: Add when we add 2FA
-          // setSuccess(data?.success);
+          setSuccess(data?.success);
         })
     })
 
