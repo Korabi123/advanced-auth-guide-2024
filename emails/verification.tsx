6669073d90f9183bcdad76fcf41de74f36b2c09d
Korab <korabimeri91@gmail.com>
2024-01-02 13:09:28 +0100

    style:created email verification teamplate

---
+++ emails/verification.tsx
@@ -0,0 +1,117 @@
+import {
+  Body,
+  Container,
+  Column,
+  Head,
+  Heading,
+  Html,
+  Img,
+  Preview,
+  Row,
+  Section,
+  Text,
+} from '@react-email/components';
+import * as React from 'react';
+
+interface VerificationEmailProps {
+  confirmLink: string;
+  name: string;
+}
+export const VerificationEmail = ({
+  confirmLink,
+  name
+}: VerificationEmailProps) => {
+
+  return (
+    <Html>
+      <Head />
+      <Preview>Auth email verification</Preview>
+      <Body style={main}>
+        <Container>
+          <Section style={logo}>
+            <Img src={`https://korabi-ecommerce-admin.vercel.app/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdncmjp41z%2Fimage%2Fupload%2Fv1704195942%2Fzxr7vlhj7zsvffcclohc.png&w=1920&q=75`} />
+          </Section>
+
+          <Section style={content}>
+            <Img width={620} src={`https://korabi-ecommerce-admin.vercel.app/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdncmjp41z%2Fimage%2Fupload%2Fv1704195773%2Fskhemiousn5nzs3vib1d.png&w=1920&q=75`} />
+
+            <Row style={{ ...boxInfos, paddingBottom: '0' }}>
+              <Column>
+                <Heading
+                  style={{
+                    fontSize: 32,
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                  }}
+                >
+                  Hi {name},
+                </Heading>
+                <Heading
+                  as="h2"
+                  style={{
+                    fontSize: 26,
+                    fontWeight: 'bold',
+                    textAlign: 'start',
+                  }}
+                >
+                  We noticed a recent attempt to Login / Register with your email.
+                </Heading>
+
+                <Text style={paragraph}>
+                  If this was you, click <a href={confirmLink}>here</a>
+                </Text>
+                <Text style={{ ...paragraph, marginTop: -5 }}>
+                  If this wasn't you, please ignore this email.
+                </Text>
+              </Column>
+            </Row>
+          </Section>
+
+          <Section style={containerImageFooter}>
+            <Img width={620} src={`https://korabi-ecommerce-admin.vercel.app/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdncmjp41z%2Fimage%2Fupload%2Fv1704195824%2Fpcgb9zsaecqlryiuwifi.png&w=1920&q=75`} />
+          </Section>
+
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 12,
+              color: 'rgb(0,0,0, 0.7)',
+            }}
+          >
+            © 2024 | 🔐 Auth 
+          </Text>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+export default VerificationEmail;
+
+const main = {
+  backgroundColor: '#fff',
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+};
+
+const paragraph = {
+  fontSize: 16,
+};
+
+const logo = {
+  padding: '30px 20px',
+};
+
+const content = {
+  border: '1px solid rgb(0,0,0, 0.1)',
+  borderRadius: '3px',
+  overflow: 'hidden',
+};
+
+const boxInfos = {
+  padding: '20px 40px',
+};
+
+const containerImageFooter = {
+  padding: '45px 0 0 0',
+};
