02334b0d966b789378bbae9264b57b995efc6339
Korab <korabimeri91@gmail.com>
2024-01-02 13:12:08 +0100

    refactor:verification token related actions

---
+++ data/verification-token.ts
@@ -0,0 +1,33 @@
+import { db } from "@/lib/db";
+
+export const getVerificationTokenByToken = async (
+  token: string
+) => {
+  try {
+    const verificationToken = await db.verificationToken.findUnique({
+      where: {
+        token,
+      }
+    });
+
+    return verificationToken;
+  } catch (error) {
+    return null;
+  }
+}
+
+export const getVerificationTokenByEmail = async (
+  email: string
+) => {
+  try {
+    const verificationToken = await db.verificationToken.findFirst({
+      where: {
+        email,
+      }
+    });
+
+    return verificationToken;
+  } catch (error) {
+    return null;
+  }
+}
