88f9e6c8087f8b232815b0155ab973c297773dce
Korab <korabimeri91@gmail.com>
2024-01-06 14:58:12 +0100

    refactor:created a new password schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,19 @@
 import * as z from "zod";
 
+export const NewPasswordSchema = z.object({
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters",
+  }).regex(new RegExp("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%&]).{8,32}"), {
+    message: "Password must contain one uppercase letter, one lowercase letter, one number and one of the following characters: * . ! @ $ % &",
+  }),
+})
+
+export const ResetSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+})
+
 export const LoginSchema = z.object({
   email: z.string().email({
     message: "Email is required",
