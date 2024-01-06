e3ba69815dfd86b4ed95e0986f7a344ea55c3e89
Korab <korabimeri91@gmail.com>
2024-01-06 14:57:27 +0100

    feat:added the ability to generate a new password reset token

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,11 +1,12 @@
 import { getVerificationTokenByEmail } from "@/data/verification-token";
+import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
 import { db } from "@/lib/db";
 
 import { v4 as uuidv4 } from "uuid";
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
-  const expires = new Date(new Date().getTime() + 3600 * 1000 );
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
 
   const existingToken = await getVerificationTokenByEmail(email);
 
@@ -26,4 +27,29 @@ export const generateVerificationToken = async (email: string) => {
   });
 
   return verificationToken;
+}
+
+export const generateResetPasswordToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getPasswordResetTokenByEmail(email);
+
+  if (existingToken) {
+    await db.passwordResetToken.delete({
+      where: {
+        id: existingToken.id,
+      }
+    })
+  }
+
+  const passwordResetToken = await db.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires
+    }
+  });
+
+  return passwordResetToken;
 }
