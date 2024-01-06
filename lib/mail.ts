311134c96276abfe009fb3b644315f9e6c1c7afa
Korab <korabimeri91@gmail.com>
2024-01-06 14:57:00 +0100

    refactor:added sendPasswordResetEmail function

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,3 +1,4 @@
+import PasswordResetEmail from "@/emails/password-reset";
 import VerificationEmail from "@/emails/verification";
 import { Resend } from "resend";
 
@@ -11,5 +12,16 @@ export const sendVerificationEmail = async (email: string, token: string, name: 
     to: email,
     subject: "Verify your email",
     react: VerificationEmail({ confirmLink: confirmLink, name: name })
+  })
+}
+
+export const sendPasswordResetEmail = async (email: string, token: string, name: string) => {
+  const resetPasswordLink = `http://localhost:3000/auth/new-password?token=${token}`;
+
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "Reset your password",
+    react: PasswordResetEmail({ resetPasswordLink: resetPasswordLink, name: name })
   })
 }
