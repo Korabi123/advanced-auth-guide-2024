df34652a2298daf1118e010a2ee7add9d07e489e
Korab <korabimeri91@gmail.com>
2024-01-07 17:38:21 +0100

    refactor:created a function that generates a 2fa token

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,8 +1,34 @@
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 import { db } from "@/lib/db";
 
+import crypto from "crypto";
 import { v4 as uuidv4 } from "uuid";
+
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+  // TODO: later change to 15mins
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+
+  if (existingToken) {
+    await db.twoFactorToken.delete({
+      where: { id: existingToken.id }
+    })
+  };
+
+  const twoFactorToken = db.twoFactorToken.create({
+    data: { 
+      token,
+      email,
+      expires
+    }
+  })
+
+  return twoFactorToken;
+}
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
