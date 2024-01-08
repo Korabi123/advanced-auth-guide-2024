9a2e89292a350f80de16348b60e6c5b50b31f376
Korab <korabimeri91@gmail.com>
2024-01-08 18:30:50 +0100

    refactor:changed 2fa token expire time to five minutes

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -8,8 +8,7 @@ import { v4 as uuidv4 } from "uuid";
 
 export const generateTwoFactorToken = async (email: string) => {
   const token = crypto.randomInt(100_000, 1_000_000).toString();
-  // TODO: later change to 15mins
-  const expires = new Date(new Date().getTime() + 3600 * 1000);
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
 
   const existingToken = await getTwoFactorTokenByEmail(email);
 
