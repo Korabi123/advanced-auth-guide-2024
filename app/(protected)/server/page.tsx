fbd8913046a2eb80af8e0389b4a758ece3bd0873
Korab <korabimeri91@gmail.com>
2024-01-09 17:55:29 +0100

    other:created an example which fetches the user data in a server component directly from the currently logged in user's session

---
+++ app/(protected)/server/page.tsx
@@ -0,0 +1,15 @@
+import { UserInfo } from "@/components/user-info";
+import { currentUser } from "@/lib/auth"
+
+const ServerPage = async () => {
+  const user = await currentUser();
+
+  return (
+    <UserInfo 
+      label="💻Server Component"
+      user={user}
+    />
+  );
+}
+ 
+export default ServerPage;
