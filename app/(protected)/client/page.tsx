7030c2e57015c362415d5f43d3f7a12483e77014
Korab <korabimeri91@gmail.com>
2024-01-09 17:54:44 +0100

    other:created an example which fetches the user info in a client component using a custom build hook.

---
+++ app/(protected)/client/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { UserInfo } from "@/components/user-info";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+const ClientPage = () => {
+  const user = useCurrentUser();
+
+  return (
+    <UserInfo 
+      label="📲Client Component"
+      user={user}
+    />
+  );
+}
+ 
+export default ClientPage;
